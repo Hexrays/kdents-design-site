3ca54fac4cef2792c595c08c21f7c207dd4194f2
Jeffrey Denton <jeffd@hexrays.at>
2016-09-25 21:58:37 -0400

    Added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,15 @@
 /* jshint devel:true */
-console.log('Look at app/js/main.js');
+import Wallop from 'wallop';
+
+
+const slider = document.querySelector('.Wallop');
+const wallop = new Wallop(slider);
+
+console.log(wallop);
+
+const totalSlildes = 6;
+let currentSlide = 0;
+
+setInterval(() => {
+  wallop.next();
+}, 5000);
