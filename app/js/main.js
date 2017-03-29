7b19d470ad971ae0594f49e0743dcb1c1abab253
Jeff Denton <jeffd@hexrays.at>
2017-03-28 23:36:16 -0400

    :art: built redesigned site

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 /* jshint devel:true */
-import Slider from './slider';
+// import Slider from './slider';
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    Slider();
+    // Slider();
   }
 };
